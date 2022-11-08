30ce02ef1d0a97bbb954499554e3c45193f398d0
Gao <3403748384@qq.com>
2022-11-08 20:22:37 +0800

    2022年11月3日

---
+++ ch01/tacos/src/main/java/tacos/HomeController.java
@@ -0,0 +1,11 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    public String home(){
+        return "home";
+    }
+}
